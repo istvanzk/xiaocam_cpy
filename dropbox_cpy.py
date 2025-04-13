e0d3c3f75e3cce80bf199578a4974d207657b52b
Istvan Z. Kovacs <istvanzk@gmail.com>
2025-04-13 15:19:24 +0200

    Correct class name

--- dropbox_cpy.py
+++ dropbox_cpy.py
@@ -26,7 +26,7 @@
 #     OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 #     WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-__all__ = ["Dropbox"]
+__all__ = ["DropboxAPI"]
 
 import rtc
 import time
